c982c9115128790528856f800680ca56617ce78c
jycouet <jycouet@gmail.com>
2023-03-10 17:12:59 +0100

    :tada: NEW: init

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://mock.shop/api"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
---
+++ src/routes/a/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { PageData } from "./$houdini";
+
+  export let data: PageData;
+
+  $: ({ Product } = data);
+</script>
+
+<pre>{JSON.stringify($Product.data, null, 2)}</pre>
